0fe675990a7814ca688d95d2810f1e90c40d5328
leesubindlqslek <rftnqls@naver.com>
2025-02-05 21:41:47 +0900

    헤헤~

--- Source/HWProject7/Public/Sparta_Character.h
+++ Source/HWProject7/Public/Sparta_Character.h
@@ -1,26 +1,2 @@
-#pragma once
-
-#include "CoreMinimal.h"
-#include "GameFramework/Character.h"
-#include "Sparta_Character.generated.h"
-
-class USprintArmComponent;
-class UCameraComponent;
-
-UCLASS()
-class HWPROJECT7_API ASparta_Character : public ACharacter
-{
-	GENERATED_BODY()
-
-public:
-	ASparta_Character();
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
-	USprintArmComponent* SpringArmCom;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
-	UCameraComponent* CameraCom;
-
-protected:
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
