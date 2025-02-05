03258158a9460cae1d4783990977fed5d33d3543
leesubindlqslek <rftnqls@naver.com>
2025-02-05 21:50:21 +0900

    BeginPlay() 함수에 디버깅용 로직 추가.
    깃 충돌 처리를 위해 디버깅 로직을 추가함.

--- Source/HWProject7/Public/Sparta_Character.h
+++ Source/HWProject7/Public/Sparta_Character.h
@@ -1,2 +1,19 @@
+// MyActor.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class TESTPROJECT_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMyActor();
+
+	virtual void BeginPlay() override;
 
 };
